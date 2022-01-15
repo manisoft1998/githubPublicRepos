fb85d46b595b18a07467eb678364812e77828ef5
manisoft1998 <86530291+manisoft1998@users.noreply.github.com>
2022-01-15 16:26:31 +0530

    UI completed

---
+++ app/src/main/java/com/org/githubrepo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.org.githubrepo
+
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
